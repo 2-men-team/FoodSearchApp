9949e9b6ba033dd4bc69c1f43b743d779a227493
Vasniktel <vasniktel@gmail.com>
2018-12-16 16:04:55 +0200

    Small changes

--- src/ua/kpi/restaurants/Main.java
+++ src/ua/kpi/restaurants/Main.java
@@ -1,6 +1,9 @@
 package ua.kpi.restaurants;
 
 import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Help.Visibility;
+import picocli.CommandLine.Option;
 import ua.kpi.restaurants.data.DataBase;
 import ua.kpi.restaurants.logic.common.algorithms.SimilaritySet;
 import ua.kpi.restaurants.logic.representation.Location;
@@ -16,6 +19,44 @@ import ua.kpi.restaurants.test.Test;
 import java.util.Set;
 
 public final class Main {
+  @Command(description = {
+      "%nSimple solution that has Server, Client and Test modes%n",
+      "Do not forget to specify properties file using%n" +
+          "'-Dua.kpi.restaurants.Config.properties=<path-to-file>'%n"
+  })
+  private static final class Arguments {
+    @Option(names = {"-p", "--port"}, description = "Server port", showDefaultValue = Visibility.ALWAYS)
+    private int port = 9991;
+
+    @Option(
+        names = {"-m", "--mode"},
+        description = {"Program mode", "  Available modes: ${COMPLETION-CANDIDATES}"},
+        showDefaultValue = Visibility.ALWAYS
+    )
+    private Mode mode = Mode.SERVER;
+
+    @Option(names = {"-h", "--help"}, usageHelp = true)
+    private boolean helpRequested;
+
+    int getPort() {
+      return port;
+    }
+
+    Mode getMode() {
+      return mode;
+    }
+
+    boolean isHelpRequested() {
+      return helpRequested;
+    }
+
+    enum Mode {
+      SERVER,
+      CLIENT,
+      TEST
+    }
+  }
+
   public static void main(String[] args) {
     Arguments arguments = CommandLine.populateCommand(new Arguments(), args);
 
